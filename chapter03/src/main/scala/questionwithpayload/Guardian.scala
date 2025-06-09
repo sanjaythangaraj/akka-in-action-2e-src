41bd8f947741d456967b5439a4ab070e0bd9770b
sanjaythangaraj <sanjaythangaraj@outlook.in>
2025-06-09 18:10:11 +0530

    manager-workers actor system comm. by adapter, ask, ask with payload

---
+++ chapter03/src/main/scala/questionwithpayload/Guardian.scala
@@ -0,0 +1,21 @@
+package questionwithpayload
+
+import akka.actor.typed.{ ActorRef, Behavior }
+import akka.actor.typed.scaladsl.Behaviors
+
+object Guardian {
+
+  sealed trait Command
+  final case object Start extends Command
+
+  def apply(): Behavior[Command] =
+    Behaviors.setup { context =>
+      val manager: ActorRef[Manager.Command] =
+        context.spawn(Manager(), "manager-1")
+
+      Behaviors.receiveMessage { message =>
+        manager ! Manager.Delegate(List("text-a", "text-b", "text-c"))
+        Behaviors.same
+      }
+    }
+}
