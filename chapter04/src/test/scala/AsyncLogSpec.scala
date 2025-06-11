1f8315cc59b5c4a649371635083f27c25d461951
sanjaythangaraj <sanjaythangaraj@outlook.in>
2025-06-11 12:11:21 +0530

    chap4 loggingspec

---
+++ chapter04/src/test/scala/AsyncLogSpec.scala
@@ -0,0 +1,45 @@
+import akka.actor.testkit.typed.scaladsl.{
+  LoggingTestKit,
+  ScalaTestWithActorTestKit
+}
+import akka.actor.typed.Behavior
+import akka.actor.typed.scaladsl.Behaviors
+import common.SimplifiedManager
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpecLike
+import org.slf4j.event.Level
+
+class AsyncLogSpec
+    extends ScalaTestWithActorTestKit
+    with AnyWordSpecLike
+    with Matchers {
+
+  "a Simplified Manager" must {
+    "be able to log 'it's done'" in {
+      val manager = testKit.spawn(SimplifiedManager(), "manager")
+
+      LoggingTestKit.info("it's done").expect {
+        manager ! SimplifiedManager.Log
+      }
+    }
+  }
+
+  "a simple behavior" must {
+    "log messages to dead letters" in {
+      val behavior: Behavior[String] =
+        Behaviors.stopped
+
+      val carl = spawn(behavior, "carl")
+
+      LoggingTestKit.empty
+        .withLogLevel(Level.INFO)
+        .withMessageRegex(
+          ".*Message.*to.*carl.*was not delivered.*2.*dead letters encountered")
+        .expect {
+          carl ! "Hello"
+          carl ! "Hello"
+        }
+    }
+  }
+
+}
