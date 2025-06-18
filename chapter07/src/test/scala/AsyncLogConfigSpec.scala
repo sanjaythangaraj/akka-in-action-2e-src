440365a678796a2891cdb93dac79c3d340eab9fb
sanjaythangaraj <sanjaythangaraj@outlook.in>
2025-06-18 17:55:26 +0530

    chap7 config example test

---
+++ chapter07/src/test/scala/AsyncLogConfigSpec.scala
@@ -0,0 +1,66 @@
+import akka.actor.testkit.typed.scaladsl.{
+  LoggingTestKit,
+  ScalaTestWithActorTestKit
+}
+import akka.actor.typed.Behavior
+import akka.actor.typed.scaladsl.Behaviors
+import com.typesafe.config.ConfigFactory
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpecLike
+
+class AsyncLogConfigSpec
+    extends ScalaTestWithActorTestKit(
+      ConfigFactory
+        .parseString(
+          """akka.eventsourced-entity.journal-enabled = false""")
+        .withFallback(ConfigFactory.load("in-memory")))
+    with AnyWordSpecLike
+    with Matchers {
+
+  "Actor" must {
+
+    "log in debug the content when receiving message" in {
+      val loggerBehavior: Behavior[String] = Behaviors.receive {
+        (context, message) =>
+          message match {
+            case message: String =>
+              context.log.debug(s"message '$message', received")
+              Behaviors.same
+          }
+      }
+
+      val loggerActor = spawn(loggerBehavior)
+      val message = "hi"
+
+      LoggingTestKit.debug(s"message '$message', received").expect {
+        loggerActor.ref ! message
+      }
+    }
+
+    "life one property from conf" in {
+      val inmemory = testKit.system.settings.config
+      val journalenabled =
+        inmemory.getString("akka.eventsourced-entity.journal-enabled")
+      val readjournal =
+        inmemory.getString("akka.eventsourced-entity.read-journal")
+
+      val loggerBehavior: Behavior[String] = Behaviors.receive {
+        (context, message) =>
+          message match {
+            case message: String =>
+              context.log.info(s"$journalenabled $readjournal")
+              Behaviors.same
+          }
+      }
+
+      val loggerActor = spawn(loggerBehavior)
+      val message = "anymessage"
+
+      LoggingTestKit
+        .info("false inmem-read-journal")
+        .expect {
+          loggerActor.ref ! message
+        }
+    }
+  }
+}
